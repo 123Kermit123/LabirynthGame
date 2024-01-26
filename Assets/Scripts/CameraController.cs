83842d346569980e580cb26b777e28629013592e
123Kermit123 <mikolax2010@gmail.com>
2024-01-26 17:25:59 +0100

    AddedCameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]private float Sensitivity = 100f;
+
+    private Transform PlayerBody;
+    private float XRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        PlayerBody = transform.parent;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * Sensitivity * Time.deltaTime;
+
+        XRotation -= mouseY;
+        XRotation = Mathf.Clamp(XRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(XRotation, 0f, 0f);
+
+        PlayerBody.Rotate(Vector3.up * mouseX);
+    }
+}
