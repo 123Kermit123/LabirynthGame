2e940f6f716acab661d5d8e5920444e3ff347690
123Kermit123 <mikolax2010@gmail.com>
2024-02-09 16:54:08 +0100

    Added Pickup and Pause Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager Instance;
+
+    [SerializeField] int TimeToEnd;
+
+    public bool GamePaused;
+    bool Endgame = false;
+    public bool win;
+
+    public GameObject MenuPanel;
     void Start()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        if (TimeToEnd <= 0)
+        {
+            TimeToEnd = 100;
+        }
+
+        InvokeRepeating(nameof(Stoper), 2, 1);
     }
+
+    public KeyCode PauseKey;
 
     // Update is called once per frame
     void Update()
     {
-        
+        MenuPanel.SetActive(GamePaused);
+
+        PauseCheck();
+    }
+
+    void Stoper()
+    {
+        TimeToEnd--;
+        Debug.Log($"Time: {TimeToEnd}");
+
+        if (TimeToEnd <= 0)
+        {
+            TimeToEnd = 0;
+            Endgame = true;
+        }
+    }
+
+    void PauseCheck()
+    {
+        if (Input.GetKeyDown(PauseKey))
+        {
+            if (GamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    public void PauseGame()
+    {
+        Cursor.lockState = CursorLockMode.None;
+
+        Debug.Log("Game Paused");
+        Time.timeScale = 0;
+
+        GamePaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        Debug.Log("Game Resumed");
+        Time.timeScale = 1;
+
+        GamePaused = false;
     }
 }
