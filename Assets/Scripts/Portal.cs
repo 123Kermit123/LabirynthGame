2c5168db0f7e14c438ca00ced254a70fb947d5c0
123Kermit123 <mikolax2010@gmail.com>
2024-02-23 17:30:53 +0100

    Fixed portal's camera problem

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -15,12 +15,33 @@ public class Portal : MonoBehaviour
     private GameObject player;
     private PortalTeleport portalTeleport;
 
+    private PortalCamera portalCamera;
+
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         portalTeleport = portalCollider.GetComponent<PortalTeleport>();
         portalTeleport.player = player.transform;
         portalTeleport.receiver = otherPortal.portalCollider;
+
+        portalCamera = myCamera.GetComponent<PortalCamera>();
+        portalCamera.playerCamera = player.GetComponentInChildren<Camera>().transform;
+        portalCamera.otherPortal = otherPortal.transform;
+        portalCamera.portal = transform; 
+
+        renderSurface.GetComponent<Renderer>().material = Instantiate(material);
+
+        if (myCamera.targetTexture != null)
+        {
+            myCamera.targetTexture.Release();
+        }
+
+        myCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+    }
+
+    private void Start()
+    {
+        renderSurface.GetComponent<Renderer>().material.mainTexture = otherPortal.myCamera.targetTexture;
     }
 
 }
--- Assets/Scripts/PortalCamera.cs
+++ Assets/Scripts/PortalCamera.cs
@@ -10,7 +10,24 @@ public class PortalCamera : MonoBehaviour
 
     void Update()
     {
-
+        PortalCameraCalculation();
     }
 
+    void PortalCameraCalculation()
+    {
+        Vector3 distanceBetweenPortalPlayer = playerCamera.position - otherPortal.position;
+
+        transform.position = portal.position + distanceBetweenPortalPlayer;
+
+        float roznicaKatowaDwochPortali = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion roznicaRotacjiPortali = Quaternion.AngleAxis(roznicaKatowaDwochPortali, Vector3.up);
+
+        Vector3 nowyKierunekKamery = roznicaRotacjiPortali * playerCamera.forward;
+
+        nowyKierunekKamery = new Vector3(nowyKierunekKamery.x * -1, nowyKierunekKamery.y, nowyKierunekKamery.z * -1);
+
+        transform.rotation = Quaternion.LookRotation(nowyKierunekKamery, Vector3.up);
+    }
 }
+
